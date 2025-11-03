b0232ac101e861ca573e085c24c1a0755e600d07
Dmitriy-py <80920815+Dmitriy-py@users.noreply.github.com>
2025-11-03 15:22:08 +0300

    Create variables.tf

---
+++ Задание №5/module/db_cluster/variables.tf
@@ -0,0 +1,6 @@
+variable "cluster_name" { type = string }
+variable "network_id" { type = string }
+variable "zone" { type = string }
+variable "ha" { type = bool }
+variable "subnet_id" { type = string }
+variable "subnet_id_ha" { type = string }
