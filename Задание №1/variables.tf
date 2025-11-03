2d82fd7695dffdcc71eea9a838ad5ae9ca5599f8
Dmitriy-py <80920815+Dmitriy-py@users.noreply.github.com>
2025-11-03 11:50:52 +0300

    Update variables.tf

--- Задание №1/variables.tf
+++ Задание №1/variables.tf
@@ -1,11 +1,5 @@
-variable "cloud_id" {type                = string}
-variable "folder_id" {type               = string}
-variable "zone" {type                     = string
-  default = "ru-central1-a"
-}
-variable "public_key_path" {type          = string
-  default = "~/.ssh/id_rsa.pub" 
-}
-variable "service_account_key_file" {type = string
-  default = "authorized_key.json"
-}
+variable "cloud_id" { type = string }
+variable "folder_id" { type = string }
+variable "zone" { default = "ru-central1-a" }
+variable "public_key_path" { default = "/home/user/.ssh/id_rsa.pub" }
+variable "sa_key_path" { default = "authorized_key.json" }
