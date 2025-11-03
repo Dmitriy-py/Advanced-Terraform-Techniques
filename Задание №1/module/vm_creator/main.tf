1742390603272fa8e79518c03bf94e75962ccf58
Dmitriy-py <80920815+Dmitriy-py@users.noreply.github.com>
2025-11-03 11:56:43 +0300

    Create main.tf

---
+++ Задание №1/module/vm_creator/main.tf
@@ -0,0 +1,31 @@
+resource "yandex_compute_instance" "vm" {
+  name        = var.vm_name
+  zone        = var.zone
+  hostname    = var.vm_hostname
+  labels      = var.instance_labels
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8l4jle6f83dt7hsv30" # Debian 12
+      size     = 10
+    }
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = var.cloud_init_content
+  }
+}
+
+output "vm" { value = yandex_compute_instance.vm }
+output "external_ip" { value = yandex_compute_instance.vm.network_interface[0].nat_ip_address }
